7fbd35dbb3c60e5529d7f1612492e6a591db9263
Bryce Carson <bcars268@mtroyal.ca>
2023-12-07 14:00:17 -0700

    Update compiled files

--- A3.sql
+++ A3.sql
@@ -1,19 +1,28 @@
-CREATE TABLE USER (
-    Email VARCHAR(21,844) CHARACTER SET utf8 PRIMARY KEY
-        CHECK Email LIKE "%@%",
+CREATE OR REPLACE TABLE USER (
+    Email VARCHAR(256) CHARACTER SET utf8 PRIMARY KEY CHECK (Email LIKE "%@%"),
     DateAdded DATETIME NOT NULL, -- not user modifiable.
-    NickName VARCHAR(21,844) CHARACTER SET utf8 UNIQUE,
-    Profile VARCHAR(21,844) CHARACTER SET utf8
+    NickName VARCHAR(256) CHARACTER SET utf8 UNIQUE,
+    Profile VARCHAR(256) CHARACTER SET utf8
 );
+CREATE OR REPLACE TRIGGER afterCreateRow_setDateAddedToNow
+BEGIN
+UPDATE USER
+SET DateAdded = CURDATE();
+END;
 
+CREATE OR REPLACE TRIGGER afterUpdateRow_preventDateAddedModification
+BEGIN
+UPDATE USER
+SET new.DateAdded = old.DateAdded
+END;
 /* Warn database clients that deleting users is impossible manually; they must
 use the provided function. */
-
 CREATE TABLE BOOK (
   BookID INT PRIMARY KEY AUTO_INCREMENT,
-  Title VARCHAR(255) NOT NULL,
-  Year INT CHECK (Year >= -3500 AND
-                  Year <= YEAR(CURDATE())),
+  Title VARCHAR(256) CHARACTER SET utf8 NOT NULL,
+  -- Year is additionally constrained by a trigger to be
+  -- <= YEAR(CURDATE()).
+  Year INT CHECK (Year >= -3500),
   NumRaters INT DEFAULT 0,
   Rating DECIMAL(2,1) DEFAULT 0.0
       CHECK (Rating >= 0.0 AND Rating <= 5.0)
@@ -25,28 +34,45 @@ CREATE TABLE BOOK (
 CREATE TRIGGER beforeDeleteRow_warnClientImpossibleChange
     BEFORE DELETE ON BOOK INSTEAD OF DELETE AS
     BEGIN
-        RAISEERROR('BOOKS cannot be deleted, only updated.')
-        ROLLBACK;
+        -- NOP: TODO: implement the trigger; use rollback?
     END;
+CREATE OR REPLACE TRIGGER beforeCreateRow_constrainYearToPresent
+BEGIN
+IF Year > YEAR(CURDATE())
+SET Year = YEAR(CURDATE())
+END;
 CREATE TABLE AUTHOR (
     AuthorID INT PRIMARY KEY AUTO_INCREMENT,
-    Lastname VARCHAR(256) CHARACTER SET utf8,
-    FirstName VARChar(256) CHARACTER SET utf8 NOT NULL,
-    MiddleName VARCHAR(256) CHARACTER SET utf8
+    FirstName VARCHAR(256) CHARACTER SET utf8 NOT NULL,
+    MiddleName VARCHAR(256) CHARACTER SET utf8,
+    Lastname VARCHAR(256) CHARACTER SET utf8
 );
 CREATE TABLE BOOKAUTHOR (
-    AuthorID INT FOREIGN KEY REFERENCES AUTHOR(AuthorID),
-    BookID INT FOREIGN KEY REFERENCES BOOK(BookID)
+    AuthorID INT,
+    BookID INT
 );
+ALTER TABLE BOOKAUTHOR DROP PRIMARY KEY;
+
+-- Composite primary key composed of foreign keys:
+ALTER TABLE BOOKAUTHOR ADD CONSTRAINT
+    PRIMARY KEY (AuthorID, BookID);
+
+ALTER TABLE BOOKAUTHOR ADD CONSTRAINT
+FOREIGN KEY (AuthorID) REFERENCES AUTHOR (AuthorID)
+ON DELETE CASCADE;
+
+-- Books cannot be deleted, so do not permit cascading.
+ALTER TABLE BOOKAUTHOR ADD CONSTRAINT
+FOREIGN KEY (BookID) REFERENCES BOOK (BookID);
 CREATE TABLE READBOOK (
-    BookID INT FOREIGN KEY REFERENCES BOOK(BookID) NOT NULL,
-    Email VARCHAR(...) FOREIGN KEY REFERENCES USER(Email)
-    NOT NULL
-    ON DELETE CASCADE,
+    BookID INT NOT NULL FOREIGN KEY REFERENCES BOOK(BookID),
+    Email VARCHAR(256) CHARACTER SET utf8 NOT NULL
+        FOREIGN KEY REFERENCES USER(Email)
+        ON DELETE CASCADE,
+    -- Constaint DateRead to be less than YEAR(CURDATE()).
     DateRead DATE NOT NULL,
-    Rating INT(2) CHECK Rating >= 1 AND Rating <= 10 NOT NULL,
-    PRIMARY KEY (BookID, Email)
-    ) ENGINE=INNODB;
+    Rating INT(2) NOT NULL CHECK (Rating >= 1 AND Rating <= 10))
+    ENGINE=INNODB;
 
 -- Include the following code
 CREATE TRIGGER afterUpdateRow_recalculateBookRating
@@ -64,10 +90,12 @@ CREATE OR REPLACE PROCEDURE CalculateRating
     UPDATE BOOK
     SET Rating = (SELECT AVG(Rating)
                   FROM READBOOK
-                  WHERE BOOK.BookID = ReadBook.BookID),
+                  WHERE BOOK.BookID = READBOOK.BookID),
     SET NumRaters = (SELECT COUNT(*)
                      FROM READBOOK
-                     WHERE BOOK.BookID = ReadBook.BookID)
-    WHERE BOOK.BookID = ReadBook.BookID;
+                     WHERE BOOK.BookID = READBOOK.BookID)
+    WHERE BOOK.BookID = READBOOK.BookID;
     END;
 
+ALTER TABLE READBOOK ADD CONSTRAINT
+PRIMARY KEY (BookID, Email);
