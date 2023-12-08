71ac811f1136dbafeaf8883e3951311b6fe0862a
Bryce Carson <bcars268@mtroyal.ca>
2023-12-07 19:05:48 -0700

    Update binaries and add DELETE_USER

--- A3.sql
+++ A3.sql
@@ -28,8 +28,25 @@ FOR EACH ROW
 -- Make use of the behaviour of trigger errors to prevent the row deletion.
 -- https://mariadb.com/kb/en/trigger-overview/#trigger-errors
 -- https://mariadb.com/kb/en/signal/
+IF PERMIT_DEL_NOT_OVERRIDDEN THEN
 SIGNAL SQLSTATE '45000' SET
 MESSAGE_TEXT = 'Users must not be deleted manually; use the DELETE_USER() procedure';
+END IF;
+
+SET @PROHIBIT_DEL_OVERRIDDEN = 0;
+
+CREATE
+OR REPLACE
+PROCEDURE DELETE_USER (IN param_email VARCHAR(256) CHARACTER SET utf8)
+MODIFIES SQL DATA
+COMMENT 'Delete a user from the database.'
+BEGIN
+DELETE FROM READBOOK WHERE READBOOK.Email = param_email;
+-- Ignore the usual trigger somehow.
+SET @PROHIBIT_DEL_NOT_OVERRIDDEN = 0;
+DELETE FROM USER WHERE USER.Email = param_email;
+SET @PROHIBIT_DEL_NOT_OVERRIDDEN = 1;
+END;
 
 CREATE TABLE BOOK (
   BookID INT PRIMARY KEY AUTO_INCREMENT,
@@ -65,10 +82,12 @@ CREATE TABLE AUTHOR (
     MiddleName VARCHAR(256) CHARACTER SET utf8,
     Lastname VARCHAR(256) CHARACTER SET utf8
 );
+
 CREATE TABLE BOOKAUTHOR (
     AuthorID INT,
     BookID INT
 );
+
 ALTER TABLE BOOKAUTHOR DROP PRIMARY KEY;
 
 -- Composite primary key composed of foreign keys:
@@ -82,6 +101,7 @@ ON DELETE CASCADE;
 -- Books cannot be deleted, so do not permit cascading.
 ALTER TABLE BOOKAUTHOR ADD CONSTRAINT
 FOREIGN KEY (BookID) REFERENCES BOOK (BookID);
+
 CREATE TABLE READBOOK (
     BookID INT NOT NULL FOREIGN KEY REFERENCES BOOK(BookID),
     Email VARCHAR(256) CHARACTER SET utf8 NOT NULL
@@ -92,28 +112,56 @@ CREATE TABLE READBOOK (
     Rating INT(2) NOT NULL CHECK (Rating >= 1 AND Rating <= 10))
     ENGINE=INNODB;
 
--- Include the following code
-CREATE TRIGGER afterUpdateRow_recalculateBookRating
-    AFTER UPDATE ON READBOOK FOR EACH ROW CalculateRating();
-
-CREATE TRIGGER afterInsertRow_recalculateBookRating
-AFTER INSERT ON READBOOK FOR EACH ROW CalculateRating();
-
-CREATE TRIGGER afterDeleteRow_recalculateBookRating
-    AFTER DELETE ON READBOOK FOR EACH ROW CalculateRating();
-
--- Include the following procedure after this code.
-CREATE OR REPLACE PROCEDURE CalculateRating
-    AS BEGIN
-    UPDATE BOOK
-    SET Rating = (SELECT AVG(Rating)
-                  FROM READBOOK
-                  WHERE BOOK.BookID = READBOOK.BookID),
-    SET NumRaters = (SELECT COUNT(*)
-                     FROM READBOOK
-                     WHERE BOOK.BookID = READBOOK.BookID)
-    WHERE BOOK.BookID = READBOOK.BookID;
-    END;
-
 ALTER TABLE READBOOK ADD CONSTRAINT
 PRIMARY KEY (BookID, Email);
+
+/* This view can be used to identify invalid users (users
+without an @ in their email) */
+CREATE VIEW InvalidUsers AS
+SELECT Email, NickName
+FROM
+    USER
+WHERE
+    Email NOT LIKE '%@%';
+
+
+/* This view provides details about books, including
+multiple authors if applicable */
+CREATE VIEW BookDetails AS
+SELECT B.BookID, B.Title, B.Year, B.NumRaters, B.Rating,
+       GROUP_CONCAT(A.FirstName, ' ',
+                    A.MiddleName, ' ',
+                    A.Lastname) AS Authors
+FROM
+    BOOK B
+LEFT JOIN
+    BOOKAUTHOR BA ON B.BookID = BA.BookID
+LEFT JOIN
+    AUTHOR A ON BA.AuthorID = A.AuthorID
+GROUP BY
+    B.BookID, B.Title, B.Year, B.NumRaters, B.Rating;
+
+
+-- View to display books with their average ratings
+CREATE VIEW BooksAvgRatings AS
+SELECT B.BookID, B.Title, AVG(RB.Rating) AS AverageRating
+FROM
+    BOOK B
+LEFT JOIN
+    READBOOK RB ON B.BookID = RB.BookID
+GROUP BY
+    B.BookID, B.Title;
+
+
+/* This view provides statistics about users who have read
+and rated books */
+CREATE VIEW UserBookDetails AS
+SELECT U.Email, U.NickName,
+       COUNT(RB.BookID) AS TotalBooksRead,
+       AVG(RB.Rating) AS AverageRating
+FROM
+    USER U
+LEFT JOIN
+    READBOOK RB ON U.Email = RB.Email
+GROUP BY
+    U.Email, U.NickName;
